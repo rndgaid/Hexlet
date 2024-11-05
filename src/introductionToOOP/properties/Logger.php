38b5b13f8defb22fb745ac3bbba88a9619fadd94
Alexander <rndgaid@gmail.com>
2024-11-05 10:45:34 +0200

    create class Logger.php

---
+++ src/introductionToOOP/properties/Logger.php
@@ -0,0 +1,10 @@
+<?php
+
+// Создайте класс \App\Logger с публичным свойством level равным info.
+
+namespace Hexlet\App\IntroductionToOOP\Properties;
+
+class Logger
+{
+    public string $level = 'info';
+}
